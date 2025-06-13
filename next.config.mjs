b01ee92204e395723859b2defcf4fd4f4f937d6c
Julioccps <julio191096@gmail.com>
2025-06-13 17:38:26 -0300

    Configura o projeto Next.js para deploy no GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  basePath: '/mvpi_novacao',
+  images: {
+    unoptimized: true,
+  },
+};
 
 export default nextConfig;
